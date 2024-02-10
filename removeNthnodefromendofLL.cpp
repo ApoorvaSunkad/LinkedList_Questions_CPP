2a77fa3366ae6bffa4cad8c590679c38759d95a1
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-02-10 10:50:18 +0530

    GFG

--- removeNthnodefromendofLL.cpp
+++ removeNthnodefromendofLL.cpp
@@ -111,3 +111,91 @@ ListNode* removeNthFromEnd(ListNode* head, int n) {
     delete (delNode);
     return head;
 }
+
+
+
+
+//Another variation of this problem
+
+
+/*
+Given a linked list consisting of L nodes and given a number N. The task is to 
+find the Nth node from the end of the linked list.
+
+Example 1:
+
+Input:
+N = 2
+LinkedList: 1->2->3->4->5->6->7->8->9
+Output: 8
+Explanation: In the first example, there
+are 9 nodes in linked list and we need
+to find 2nd node from end. 2nd node
+from end is 8.  
+Example 2:
+
+Input:
+N = 5
+LinkedList: 10->5->100->5
+Output: -1
+Explanation: In the second example, there
+are 4 nodes in the linked list and we
+need to find 5th from the end. Since 'n'
+is more than the number of nodes in the
+linked list, the output is -1.
+Your Task:
+The task is to complete the function getNthFromLast() which takes two arguments:
+reference to head and N and you need to return Nth from the end or -1 in case 
+node doesn't exist.
+
+Note:
+Try to solve in a single traversal.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= L <= 106
+1 <= N <= 106
+
+
+*/
+
+
+//Optimised solution
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//T.C = O(Len)
+//S.C = O(1)
+
+int getNthFromLast(Node *head, int n){
+
+    // Your code here
+    if(head == NULL){
+        return -1;
+    }
+    int i = 0;
+    Node* slow = head;
+    Node* fast = head;
+    
+    while(fast!=NULL && i<n){
+        fast = fast->next;
+        i++;
+    }
+    
+    if(fast == NULL){
+        if(i<n){
+            return -1;
+        }
+        return slow->data;
+    }
+    
+    while(fast->next != NULL){
+        fast = fast->next;
+        slow = slow->next;
+    }
+    
+    return slow->next->data;
+}
